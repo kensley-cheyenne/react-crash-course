71ca758e508b0da99a4ff27cdbf0321dc94fb4d6
kensley-cheyenne <mkensley07@gmail.com>
2026-02-25 12:53:37 -0600

    progress

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+import Todo from "./components/Todo.jsx";
+import Title from "./components/Title.jsx";
+import Modal from "./components/Modal.jsx";
+
+function App() {
+  return (
+    <>
+      <Title />
+      <div className="todo__wrapper">
+        <Todo
+          title="Finish Frontend Simplified"
+          paragraph="Code along with Frontend Simplified step by step."
+        />
+        <Todo
+          title="Finish Interview Section"
+          paragraph="Finish every interview question in the next 6 weeks."
+        />
+        <Todo title="Land a $100k Job" paragraph="Apply to 100 jobs." />
+      </div>
+      {/* <Modal title="Are you sure?" /> */}
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Todo.jsx
@@ -0,0 +1,12 @@
+import './Todo.css';
+
+function Todo({ title, paragraph }) {
+    return (
+        <div className="todo">
+            <h2>{title}</h2>
+            <p>{paragraph}</p>
+            <button>Delete</button>
+        </div>
+    )
+}
+export default Todo;
