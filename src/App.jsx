3d577d8509d02aef8d6d17e84e4127b6d931f9c6
kensley-cheyenne <mkensley07@gmail.com>
2026-03-03 13:05:27 -0600

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,16 @@ import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import Contact from "./pages/Contact.jsx";
 import Nav from "./components/Nav.jsx";
-import Users from "./components/Users.jsx";
+import Users from "./pages/Users.jsx";
 
 function App() {
   
   return (
     <>
       <Router>
-        <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/users/:username" element={<Users />} />
+          <Route path="/users/:id" element={<Users />} />
         </Routes>
       </Router>
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,42 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import User from "../components/User.jsx";
+import { Link } from "react-router-dom";
+
 function Home() {
-    return <h1>Home</h1>;
+  const [users, setUsers] = useState([]);
+
+  async function fetchUsers() {
+    const { data } = await axios.get(
+      "https://jsonplaceholder.typicode.com/users",
+    );
+    setUsers(data);
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetchUsers();
+    }, 2000);
+  }, []);
+
+  function renderUsers() {
+    return users.map((user) => (
+      <Link to={`/users/${user.id}`} key={user.id}>
+        <User
+          id={user.id}
+          name={user.name}
+          email={user.email}
+          username={user.username}
+        />
+      </Link>
+    ));
+  }
+
+  function renderSkeletonLoading() {
+    return <h1>Loading...</h1>;
+  }
+
+  return <div>{users.length ? renderUsers() : renderSkeletonLoading()}</div>;
 }
 
 export default Home;
